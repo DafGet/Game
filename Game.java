bb9e7c06eb8dc0bb2eaf05190a2425f8ea67f993
DafGet <100050706+DafGet@users.noreply.github.com>
2022-12-22 08:23:40 -0600

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,19 +1,23 @@
+/******
+*@Programa: Ajedrez
+*Autor:    @DNAFx=y   @Dafget34
+*Fecha:    04/08/2022
+*Descripción: 
+*******/
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
 public class Game extends JFrame implements ActionListener {
-
-    public JCheckBox[] product = new JCheckBox[10];
-    public JLabel[] lb = new JLabel[10];
-    public JTextArea areaTexto;
-    public JButton[] btn =  new JButton[2];
-    public String bt[]={"Cerrar aplicacion", "Jugar"};
-    public JPasswordField pw;
+    
+    public JLabel lb; 
+    public JButton[][] cell =  new JButton[8][8];
+    public int[][] pos = new int[8][8];
+    int cpy=0, x, y, xi, yi, trn=0;
 
     public static void main(String[] args) {
         Game demo = new Game();
-        demo. setBounds(500,150,500,470);
+        demo. setBounds(500,150,1000,1000);
         demo.setVisible(true);
         demo.setTitle(" Juego ");
     }
@@ -22,25 +26,66 @@ public class Game extends JFrame implements ActionListener {
         setLayout(null);
         Container ventana = getContentPane();
 
-        lb[0] = new JLabel("En construccion...");
-        lb[0].setBounds(190,20,150,20);
-        ventana.add(lb[0]);
+        lb = new JLabel("En construccion...");
+        lb.setBounds(190,20,150,20);
+        ventana.add(lb);
 
-        for(int e=0, y=350; e<2; e++, y+=30){
-            btn[e] = new JButton(bt[e]);
-            btn[e].setBounds(185, y, 140, 30);
-            ventana.add(btn[e]);
-            btn[e].addActionListener( this); 
+        for(int y=0, y1=50; y<8; y++, y1+=99) {
+            for(int x=0, x1=15; x<8; x++, x1+=99) {
+                cell[x][y] = new JButton("");
+                cell[x][y].setBounds(x1, y1, 100, 100);
+                ventana.add(cell[x][y]);
+                cell[x][y].addActionListener( this); 
+                if(y%2==0) if(x%2!=0) cell[x][y].setBackground(Color.GRAY);
+                if(y%2!=0) if(x%2==0) cell[x][y].setBackground(Color.GRAY);
+            }
         }
+
+        for(int x=-1, val=7; x<4; x++, val--, pos[x][0]=val, pos[x][7]=val+10);
+        for(int x=4, val=3; x<7; x++, val++, pos[x][0]=val, pos[x][7]=val+10);
+        for(int x=-1; x<7; x++, pos[x][1]=1, pos[x][6]=11);        
+
+        for(int y=0; y<8; y++) {
+            for(int x=0; x<8; x++) {
+                for(int i=6; i>0; i--){
+                    if(pos[x][y]==i) cell[x][y].setIcon(new ImageIcon("pieces/"+i+".png"));
+                    if(pos[x][y]==i+10) cell[x][y].setIcon(new ImageIcon("pieces/"+(i+10)+".png"));
+                }
+            } 
+        } 
+
     }
 
     public void actionPerformed(ActionEvent event) {
-        Object origen= event.getSource();
+        Object origen= event.getSource();  
+        
+        for(int y=0; y<8; y++) {
+            for(int x=0; x<8; x++) {
+                if(cpy==0){
+                    if(origen == cell[x][y]){
+                        if(pos[x][y]==0) break;
+                        xi=x;
+                        yi=y;
+                        cpy=1;
+                    }
+                } else{
+                    if(origen == cell[x][y]){
+                        // if(pos[xi][yi]==pos[x][y]) break;
+                        cell[xi][yi].setIcon(new ImageIcon(""));
+                        cell[x][y].setIcon(new ImageIcon("pieces/"+pos[xi][yi]+".png"));
+                        pos[x][y]=pos[xi][yi];
+                        pos[xi][yi]=0;
 
-        if(origen == btn[1]) System.out.print("Loading... ");
-        else System.exit(0);
-                
-
-    
+                        cpy=0; 
+                    }
+                }
+            } 
+        } 
+        
     }
 }
+
+
+// Que no se coma a si misma
+
+// Pa que no se coman entre si podemos limitarlos por rangos :)
